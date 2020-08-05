b8a9bc61f30310db5fe3f8f678316f148a423de4
kevin <tenngawang6080@gmail.com>
2020-08-05 01:01:04 -0400

    added back button to About/mission page

--- src/container/About.js
+++ src/container/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import styled from 'styled-components';
+import { Link  } from 'react-router-dom';
+
+import { color } from '../theme';
 
 const AboutContainer = styled.div`
     width: 100%;
@@ -17,13 +19,32 @@ const AboutDescription = styled.div`
     margin: 2rem;
 `;
 
+const Text = styled.p`
+    margin-top: 2rem;
+`;
+
+const BackBtn = styled.button`
+    margin-top: 4rem;
+    width: 5rem;
+    padding: 0.5rem;
+    border-style: none;
+    background-color: ${color.lake};
+    color: ${color.white};
+    font-weight: bold;
+    border-radius: 1rem;
+`;
+
 const About = () => {
     return(
         <AboutContainer>
             <Img src="about_img.png" alt="Men Multitask" />
             <AboutDescription>
                 <h1>Mission</h1>
-                <p>Lets admit it, everyone hate the process of job search just like me and what worst in this process of keeping track of all the job you applied. Yes, you may use Google Excel or Google sheet but for me personally I hate using table and rows where If I need to find something I need to scoll throught all the item.</p>
+                <Text>Lets admit it, everyone hate the process of job search just like me and what worst in this process of keeping track of all the job you applied. Yes, you may use Google Excel or Google sheet but for me personally I hate using table and rows where If I need to find something I need to scoll throught all the item.</Text>
+
+                <Text>Solution for someone who hate/dislike table and row: Oppotunity Tracker, where user will be able to store as much of their job oppotunity and update along the way. </Text>
+
+                <Link to={"/"} style={{ textDecoration: 'none' }}><BackBtn>HOME</BackBtn></Link>
             </AboutDescription>
         </AboutContainer>
     )
